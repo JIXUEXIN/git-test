b7f35064b78fd3653091681b231ab44acfffdddc
JiXuexin <870562921@qq.com>
2022-06-29 10:35:15 +0800

    hot-fix commit1

--- src/main/java/com/jxx/pojo.java
+++ src/main/java/com/jxx/pojo.java
@@ -4,5 +4,6 @@ public class pojo {
     public static void main(String[] args) {
         System.out.println("fuck you");
         System.out.println("fuck you");
+        System.out.println("hello hot-fix");
     }
 }
